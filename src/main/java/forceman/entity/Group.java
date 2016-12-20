57987e7e90814018d9d427bf479d37403be1f933
Igor <forceman28@gmail.com>
2016-12-20 17:27:52 +0500

    Добавлены 2 класса: User и Group

---
+++ src/main/java/forceman/entity/Group.java
@@ -0,0 +1,9 @@
+package forceman.entity;
+
+/**
+ * Created by Igor on 20.12.2016.
+ */
+public class Group {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/forceman/entity/User.java
@@ -0,0 +1,15 @@
+package forceman.entity;
+
+import java.util.Date;
+
+/**
+ * Created by Igor on 20.12.2016.
+ */
+public class User {
+    private int id;
+    private String fio;
+    private Date birthday;
+    private String login;
+    private String password;
+    private boolean active;
+}
